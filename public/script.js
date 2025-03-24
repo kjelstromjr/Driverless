88bab053551975fe39d422b11adc7a7a551013ac
kjelstromjr <jeffreykjelstrom@gmail.com>
2025-03-24 10:12:41 -0400

    Fixed ports

--- public/script.js
+++ public/script.js
@@ -220,11 +220,11 @@ function restart() {
         maxCars: document.getElementById("cars").value
     }
 
-    fetch(window.location.origin + ":" + window.location.port + "/update-settings", {
+    fetch(window.location.origin + "/update-settings", {
         method: "POST",
         headers: {
             "Content-type": "application/json; charset=UTF-8",
-            "Origin": window.location.origin + ":" + window.location.port + ""
+            "Origin": window.location.origin + ""
         },
         body: JSON.stringify(data)
     }).then(res => {
@@ -306,7 +306,7 @@ async function upload() {
                 document.getElementById("uploadMessage").textContent = uploadingPhrases[Math.round(Math.random() * (uploadingPhrases.length - 1))] + "...";
             }, 10000);
 
-            const response = await fetch(window.location.origin + ":" + window.location.port + "/upload-mod", {
+            const response = await fetch(window.location.origin + "/upload-mod", {
                 method: 'POST',
                 body: formData,
             });
@@ -382,11 +382,11 @@ async function loadFile() {
 loadFile();
 
 function getData() {
-	fetch(window.location.origin + ":" + window.location.port + "/get-player-position", {
+	fetch(window.location.origin + "/get-player-position", {
         method: "POST",
         headers: {
             "Content-type": "application/json; charset=UTF-8",
-            "Origin": window.location.origin + ":" + window.location.port + ""
+            "Origin": window.location.origin + ""
         }
     }).then(res => {
         clearPlayers();
@@ -420,11 +420,11 @@ function getData() {
 }
 
 function getMaps() {
-    fetch(window.location.origin + ":" + window.location.port + "/maps", {
+    fetch(window.location.origin + "/maps", {
         method: "POST",
         headers: {
             "Content-type": "application/json; charset=UTF-8",
-            "Origin": window.location.origin + ":" + window.location.port + ""
+            "Origin": window.location.origin + ""
         }
     }).then(res => {
         if (res.ok) {
@@ -455,11 +455,11 @@ function getMaps() {
 }
 
 function getSetup() {
-    fetch(window.location.origin + ":" + window.location.port + "/current-setup", {
+    fetch(window.location.origin + "/current-setup", {
         method: "POST",
         headers: {
             "Content-type": "application/json; charset=UTF-8",
-            "Origin": window.location.origin + ":" + window.location.port + ""
+            "Origin": window.location.origin + ""
         }
     }).then(res => {
         if (res.ok) {
@@ -531,11 +531,11 @@ function closeUploadPage() {
 }
 
 function getMods() {
-    fetch(window.location.origin + ":" + window.location.port + "/mods", {
+    fetch(window.location.origin + "/mods", {
         method: "GET",
         headers: {
             "Content-type": "application/json; charset=UTF-8",
-            "Origin": window.location.origin + ":" + window.location.port + ""
+            "Origin": window.location.origin + ""
         }
     }).then(res => {
         if (res.ok) {
@@ -626,11 +626,11 @@ function changeMods() {
         disabled: deactivated
     }
 
-    fetch(window.location.origin + ":" + window.location.port + "/change-mods", {
+    fetch(window.location.origin + "/change-mods", {
         method: "POST",
         headers: {
             "Content-type": "application/json; charset=UTF-8",
-            "Origin": window.location.origin + ":" + window.location.port + ""
+            "Origin": window.location.origin + ""
         },
         body: JSON.stringify(d)
     }).then(res => {
